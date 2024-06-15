aec5080f147620c53a7c1e9bf14a76bc9e9c1321
enemancy <y.ohishicw@gmail.com>
2024-06-15 21:35:05 +0900

    install breeze

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,21 +6,23 @@
         <!--Fonts-->
         <link href="https://fonts.googleapis.com/css_family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">{{ $post->title }}</h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>内容</h3>
-                <p>{{ $post->body }}</p>
-                <h4>カテゴリー</h4>
-                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+    <x-app-layout>
+        <body>
+            <h1 class="title">{{ $post->title }}</h1>
+            <div class="content">
+                <div class="content__post">
+                    <h3>内容</h3>
+                    <p>{{ $post->body }}</p>
+                    <h4>カテゴリー</h4>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                </div>
             </div>
-        </div>
-        <div class="edit">
-            <a href="/posts/{{ $post->id }}/edit">編集</a>
-        </div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+            <div class="edit">
+                <a href="/posts/{{ $post->id }}/edit">編集</a>
+            </div>
+            <div class="footer">
+                <a href="/">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
